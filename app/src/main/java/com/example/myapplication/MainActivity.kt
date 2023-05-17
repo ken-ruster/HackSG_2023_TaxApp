a0958040f3ea25a0ef8fdf145b1c91b4c045e2ab
POeticPotatoes <poeticpotatoes@gmail.com>
2023-05-17 21:45:53 +0800

    Reset main activity to default

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,44 +1,11 @@
 package com.example.myapplication
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.TextView
-import com.example.myapplication.data.Exp
-import com.example.myapplication.data.Job
-import com.example.myapplication.data.Rev
-import com.example.myapplication.data.TaxProfile
-import com.example.myapplication.storage.FileReader
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val profile: TaxProfile = TaxProfile(
-            listOf(Job("owo", "iwi"),),
-            listOf(Rev(1, "hehehaha", 1F)),
-            listOf(Exp(2, "uwuwu", 2F, mapOf("iyi" to 3F)))
-            )
-
-        val btnSave = findViewById<Button>(R.id.btnSave)
-        val btnView = findViewById<Button>(R.id.btnView)
-        val fileData = findViewById<TextView>(R.id.editFile)
-
-        btnSave.setOnClickListener(View.OnClickListener {
-            val filename = FileReader.saveFile(profile, this.applicationContext)
-            println("Saved file as $filename")
-        })
-
-        btnView.setOnClickListener(View.OnClickListener {
-            val filename = "2023.json"
-            val profile: TaxProfile? = FileReader.readFile(filename, this.applicationContext)
-            println("File loaded")
-            val json = jacksonObjectMapper().writeValueAsString(profile)
-            fileData.text = json
-        })
     }
 }
