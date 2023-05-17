b57c0560204f2f6b1f7edbaac7ee1f0bd7aec7a4
ken_ruster <ken.ruster@gmail.com>
2023-05-17 22:53:45 +0800

    Added UI elements

--- app/src/main/java/com/example/myapplication/TaxProfile.kt
+++ app/src/main/java/com/example/myapplication/TaxProfile.kt
@@ -10,6 +10,16 @@ class TaxProfile(
     fun totalMatCost(): Float = exps.sumOf { it.amt.toDouble() * (1 - it.expType) }.toFloat()
     fun totalAllowableExp(): Float = exps.sumOf { it.amt.toDouble() * it.expType }.toFloat()
     fun totalGrossProfit() = totalRev() - totalMatCost()
+
+    fun jobString(): String{
+        var temp = jobs[0].jobName
+
+        if(jobs.lastIndex >= 1){
+            temp = temp + ", " + jobs[1].jobName
+        }
+
+        return temp
+    }
     fun totalAdjProfit() = totalGrossProfit() - totalAllowableExp()
     fun isEmpty(): Boolean {return jobs.isEmpty()}
 }
