2c03e076c97ee73c1f90f3e8b65805b73fd2636b
ken_ruster <ken.ruster@gmail.com>
2023-05-17 22:53:45 +0800

    Added UI elements

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.databinding.ActivityMainBinding
+import com.xwray.groupie.GroupieAdapter
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    var profileArray: Array<TaxProfile> = emptyArray()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val yaAdapter: GroupieAdapter = GroupieAdapter()
+        val recyclerView: RecyclerView = binding.listProfiles
+        recyclerView.adapter = yaAdapter
+
+        for(profile in profileArray){
+            yaAdapter.add(ProfileItem(profile))
+        }
+
+        setContentView(binding.root)
     }
 }
