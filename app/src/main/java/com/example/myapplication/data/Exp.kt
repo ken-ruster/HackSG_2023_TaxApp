0522ecfd2cfe4d5b7e6cfb9b1e7b8cee3ec4c041
POeticPotatoes <poeticpotatoes@gmail.com>
2023-05-17 21:42:37 +0800

    Complete file reading segment

--- app/src/main/java/com/example/myapplication/data/Exp.kt
+++ app/src/main/java/com/example/myapplication/data/Exp.kt
@@ -4,5 +4,5 @@ data class Exp (
     val expType: Int, // 0 is mat cost, 1 is allowable expense
     val expName: String,
     var amt: Float,
-    val portion: HashMap<String, Float> // <jobID, portion in %>
+    val portion: Map<String, Float> // <jobID, portion in %>
 ){}
--- app/src/main/java/com/example/myapplication/data/TaxProfile.kt
+++ app/src/main/java/com/example/myapplication/data/TaxProfile.kt
@@ -1,11 +1,12 @@
 package com.example.myapplication.data
 
 import com.fasterxml.jackson.annotation.JsonFormat
+import com.fasterxml.jackson.annotation.JsonIgnore
 
 class TaxProfile(
-    private val jobs: List<Job>,
-    private val revs: List<Rev>,
-    private val exps: List<Exp>
+    val jobs: List<Job>,
+    val revs: List<Rev>,
+    val exps: List<Exp>
 ) {
     val fy: Int = 2023
     fun totalRev(): Float = revs.sumOf { it.amt.toDouble() }.toFloat()
@@ -13,5 +14,7 @@ class TaxProfile(
     fun totalAllowableExp(): Float = exps.sumOf { it.amt.toDouble() * it.expType }.toFloat()
     fun totalGrossProfit() = totalRev() - totalMatCost()
     fun totalAdjProfit() = totalGrossProfit() - totalAllowableExp()
+
+    @JsonIgnore
     fun isEmpty(): Boolean {return jobs.isEmpty()}
 }
