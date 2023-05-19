897dac4b96df5b5c4a5a643bdbc4e5df04b4d87b
POeticPotatoes <poeticpotatoes@gmail.com>
2023-05-19 21:50:41 +0800

    Complete expenditure suggestion function

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import com.example.myapplication.data.Job
+import com.example.myapplication.data.ProfileManager
+import com.example.myapplication.data.TaxProfile
 import com.example.myapplication.storage.ExpenseDefaults
 import com.example.myapplication.storage.JobDefaults
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
@@ -12,12 +15,8 @@ import kotlin.math.exp
 
 class MainActivity : AppCompatActivity() {
 
-    private var expenseDefaults: ExpenseDefaults? = null
-    private var jobDefaults: JobDefaults? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        expenseDefaults = ExpenseDefaults(applicationContext)
-        jobDefaults = JobDefaults(applicationContext)
+        val manager = ProfileManager(applicationContext)
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -26,20 +25,25 @@ class MainActivity : AppCompatActivity() {
         val btnView = findViewById<Button>(R.id.btnView)
         val fileData = findViewById<TextView>(R.id.editFile)
 
-        fileData.text = "${expenseDefaults.toString()} ||| ${jobDefaults.toString()}"
+        var profile : TaxProfile? = null
+        val jobs = listOf(
+            Job("Private Hire Transport", "Grab"),
+            Job("Private Hire Transport", "Uber"),
+            Job("Food Delivery", "Grab Food"),
+            Job("Tuition", "The Learning Lab")
+        )
+        fileData.text = "${jacksonObjectMapper().writeValueAsString(profile)}"
 
         btnView.setOnClickListener(View.OnClickListener {
-            expenseDefaults!!.loadDefaults()
-            jobDefaults!!.loadDefaults()
-            fileData.text = "${expenseDefaults.toString()} ||| ${jobDefaults.toString()}"
-            println("Loaded defaults")
+            println("Creating new profile")
+            val newProfile = manager.createProfile(jobs, profile)
+            fileData.text = "${jacksonObjectMapper().writeValueAsString(newProfile)}"
         })
 
         btnSave.setOnClickListener(View.OnClickListener {
-            fileData.text = "${expenseDefaults.toString()} ||| ${jobDefaults.toString()}"
-            expenseDefaults?.save()
-            jobDefaults?.save()
-            println("Saved defaults")
+            println("Creating new profile")
+            profile = manager.createProfile(jobs, profile)
+            fileData.text = "${jacksonObjectMapper().writeValueAsString(profile)}"
         })
     }
 }
