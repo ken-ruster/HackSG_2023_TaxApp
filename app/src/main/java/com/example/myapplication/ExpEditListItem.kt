fdc239d742121d10d316aa145def11d4ea397dac
ken_ruster <ken.ruster@gmail.com>
2023-05-23 14:33:12 +0800

    handle proportions not exceeding total 100%, and some ui tweaks.

--- app/src/main/java/com/example/myapplication/ExpEditListItem.kt
+++ app/src/main/java/com/example/myapplication/ExpEditListItem.kt
@@ -1,11 +1,12 @@
 package com.example.myapplication
 
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.View
 import com.example.myapplication.data.Job
 import com.example.myapplication.databinding.ExpEditRowBinding
 import com.xwray.groupie.viewbinding.BindableItem
+import java.math.RoundingMode
+import kotlin.math.max
+import kotlin.math.min
 
 
 open class ExpEditListItem(
@@ -24,36 +25,51 @@ open class ExpEditListItem(
                 portionAmt.setText("0.0")
                 false
             }
+
             portionAmt.isEnabled = checkBox.isChecked
 
             checkBox.setOnCheckedChangeListener { _, isChecked ->
                 portionAmt.isEnabled = isChecked
 
-                if(isChecked) {
-                    val remainder = map.values.sum()
-                    val temp = portionAmt.text.toString().toFloatOrNull() ?: run {
-                        portionAmt.setText("0.0")
-                        0.0f
-                    }
-                    map.put(job.id, temp)
+                if (isChecked) {
+                    val remainder: Float = max(100f - map.values.sum(), 0f).round()
+                    map[job.id] = remainder
+                    portionAmt.setText(remainder.toString())
+                    root.requestFocus()
                 } else {
-                    portionAmt.setText("0.0")
                     map.remove(job.id)
+                    portionAmt.setText("0.0")
                 }
             }
 
-            portionAmt.addTextChangedListener(object: TextWatcher{
-                override fun beforeTextChanged(
-                    s: CharSequence?,
-                    start: Int,
-                    count: Int,
-                    after: Int
-                ) {}
+            portionAmt.setOnFocusChangeListener { _, on ->
+                if (on) return@setOnFocusChangeListener
+
+                val fv = portionAmt.text.toString().toFloatOrNull()?.round() ?: 0.0f
+                val remainder: Float = max(100f - map.values.sum() + (map[job.id] ?: 0f), 0f).round()
+                val minV = min(remainder, fv).round()
+                when {
+                    minV == 0f -> {
+                        checkBox.isChecked = false
+                    }
+                    minV < fv -> {
+                        map[job.id] = minV
+                        portionAmt.setText(minV.toString())
+                    }
+                    else -> {
+                        map[job.id] = minV
+                    }
+
+                }
+            }
+
+            /*portionAmt.addTextChangedListener(object: TextWatcher{
 
                 override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                     if(!s.isNullOrBlank()) {
-                        checkBox.isChecked = s.toString().toFloatOrNull()?.let {
-                            map.replace(job.id, s.toString().toFloat())
+                        checkBox.isChecked = s.toString().toFloatOrNull()?.round()?.let {
+                            map.replace(job.id, it)
+                            //portionAmt.setText(it.toString())
                             it != 0.0f
                         } ?: run {
                             map.remove(job.id)
@@ -67,11 +83,14 @@ open class ExpEditListItem(
                     }
                 }
 
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
                 override fun afterTextChanged(s: Editable?) {}
 
-            })
+            })*/
         }
     }
+
+    private fun Float.round(): Float = toBigDecimal().setScale(1, RoundingMode.UP).toFloat()
 
     override fun getLayout(): Int = R.layout.exp_edit_row
 
